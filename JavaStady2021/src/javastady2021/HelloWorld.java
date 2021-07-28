bbceeff66584d781f0663f6fedc0f06a4b82da07
Nike Lupin <nike.lupin1982@gmail.com>
2021-07-28 19:43:43 +0300

    first commit

---
+++ JavaStady2021/src/javastady2021/HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javastady2021;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author 1
+ */
+public class HelloWorld {
+    public static void main(String[]args){
+        
+        System.out.println("Hello World");
+        JOptionPane.showMessageDialog(null,"Hellow World");
+    }
+    
+    
+}
